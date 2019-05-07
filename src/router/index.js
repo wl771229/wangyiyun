d2adeff1b015c43570ae8250a87ad2ee50f417b1
wl771229 <1027175509@qq.com>
2019-05-07 18:52:17 +0800

    keep-alive

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,47 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-import Index from '@/components/index'
 
-Vue.use(Router)
+const Index = () => import('@/components/index');
+const Ranking = () => import('@/components/list/Ranking');
+const Catlist = () => import('@/components/list/Catlist');
+const CatDetail = () => import('@/components/list/CatDetail');
+
+Vue.use(Router);
 
 export default new Router({
+
+  // mode: 'history',  // 去掉路由地址的#
   routes: [
     {
       path: '/',
       name: 'Index',
-      component: Index
+      component: Index,
+      meta: {
+        keepAlive: true // 需要被缓存
+      }
+    },
+
+    {
+      path: '/ranking',
+      name: '排行榜',
+      component: Ranking,
+   
+    },
+    {
+      path: '/catlist',
+      name: '歌单',
+      component: Catlist,
+      meta: {
+        keepAlive: false // 不需要缓存
+      }
+    },
+    {
+      path: '/catdetail',
+      name: 'catdetail',
+      component: CatDetail,
+      meta: {
+        keepAlive: true // 需要被缓存
+      }
     }
   ]
 })
