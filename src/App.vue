81826efa5f0bbb56ed69795c6e310fb666f85e65
wl771229 <1027175509@qq.com>
2019-05-07 18:54:25 +0800

    keep-alive

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,20 @@
 <template>
   <div id="app">
-    <router-view/>
+    <!--<keep-alive >-->
+    <!--<router-view/>-->
+    <!--</keep-alive>-->
+
+    <keep-alive>
+      <!--这里是会被缓存的组件-->
+
+      <router-view v-if="this.$route.meta.keepAlive"></router-view>
+
+    </keep-alive>
+
+    <router-view v-if="!this.$route.meta.keepAlive"></router-view>
+
+
+
   </div>
 </template>
 
